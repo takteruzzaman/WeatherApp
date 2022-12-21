06222ce56940f6e6e688689a5d07d89ca55d7024
takteruzzaman <116463326+takteruzzaman@users.noreply.github.com>
2022-12-20 22:56:35 -0500

    Add files via upload

---
+++ WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tyler Akteruzzaman on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
